43079e09a9a65075935e50bb7ebf9eb08be0ba0a
LuYu <luyucia@gmail.com>
2017-12-06 15:58:27 +0800

    add dialect for mysql and sqlite3

--- gocatdb.go
+++ gocatdb.go
@@ -5,7 +5,7 @@ import (
 "database/sql"
 "log"
 "reflect"
-"fmt"
+// "fmt"
 )
 
 type Catdb struct{
@@ -14,9 +14,11 @@ type Catdb struct{
     tablename string
     sql string
     Columns []string
+    dialect Dialect
 }
 
-func (this *Catdb) BindDb(db *sql.DB) {
+func (this *Catdb) BindDb(db *sql.DB,dialect string) {
+    this.dialect = Dialect{Dbtype:dialect}
     this.db = db
     this.Connected = 1
 }
@@ -30,21 +32,21 @@ func (this *Catdb) Insert(data interface{}) (*Catdb){
     paramType := reflect.TypeOf(data)
     // fmt.Println(paramType)
     // fmt.Println(paramType.Kind())
+    // 根据不同类型判断执行操作
     switch paramType.Kind(){
         case reflect.Slice:
-        fmt.Println("not support")
+        if paramType.Elem().Kind() == reflect.Map{
+            if paramType.Elem().Key().Kind()==reflect.String && paramType.Elem().Elem().Kind()==reflect.Interface {
+                this.insert_map_slice(data.([]map[string]interface{}))
+            }
+
+        }
         case reflect.Map:
         if paramType.Key().Kind()==reflect.String && paramType.Elem().Kind()==reflect.Interface {
             this.insert_map(data.(map[string]interface{}))
-
         }
+
     }
-
-
-
-
-
-
 
     return this
 }
@@ -52,24 +54,7 @@ func (this *Catdb) Insert(data interface{}) (*Catdb){
 
 
 
-func GetSqliteType(i interface{}) string{
-     switch i.(type){
-        case int:
-            return "integer"
-        case float64:
-            return "float"
-        case string:
-            return "text"
-        case bool:
-            return "integer"
-        case []byte:
-            return "blob"
-        // case time.Time:
-            // return "datetime"
-        default:
-            return "text"
-    }
-}
+
 
 func (this *Catdb) Create(data map[string]interface{}) (*Catdb){
     var sql bytes.Buffer
@@ -84,7 +69,7 @@ func (this *Catdb) Create(data map[string]interface{}) (*Catdb){
         }
         sql.WriteString("`"+columnName+"`")
         sql.WriteString(" ")
-        sql.WriteString(GetSqliteType(value))
+        sql.WriteString(this.dialect.GetType(value))
         first = false
     }
     sql.WriteString(" ); ")
@@ -94,6 +79,37 @@ func (this *Catdb) Create(data map[string]interface{}) (*Catdb){
 }
 
 
+func (this *Catdb) Query(query string) ([]map[string]interface{}){
+    rows, err := this.db.Query(query)
+    if err != nil {
+        log.Fatal(err)
+    }
+    // defer rows.Close()
+    columns, _ := rows.Columns()
+    length := len(columns)
+
+    rs := []map[string]interface{}{}
+
+    for rows.Next(){
+        row := make([]string,length)
+        columnPointers := make([]interface{}, length)
+        for i:=0;i<length;i++ {
+            columnPointers[i] = &row[i]
+        }
+        rows.Scan(columnPointers...)
+        tmpmap := make(map[string]interface{})
+
+        for i:=0 ; i<length ;i++{
+            columnName := columns[i]
+            columnValue := row[i]
+            // fmt.Println(reflect.TypeOf(columnValue))
+            tmpmap[columnName] = columnValue
+
+        }
+        rs = append(rs,tmpmap)
+    }
+    return rs
+}
 
 
 func (this *Catdb) Sql() (string){
