19394e103d39d1ce26c4028a6c011bd300e33ae2
luyulewin <luyulewin@didichuxing.com>
2019-07-07 11:33:28 +0800

    '重构'

--- dialect.go
+++ dialect.go
@@ -1,63 +1,53 @@
 package gocatdb
 
-import (
-// "bytes"
-// "database/sql"
-// "log"
-// "reflect"
-// "fmt"
-)
-
 type Dialect struct{
-    Dbtype string
+	Dbtype string
 
 }
 
 
 func (this *Dialect) GetType(v interface{}) string {
-    if this.Dbtype=="sqlite3" {
-        return this.getSqliteType(v)
-    }else if this.Dbtype=="mysql"{
-        return this.getMysqlType(v)
-    }
-    return ""
+	if this.Dbtype=="sqlite3" {
+		return this.getSqliteType(v)
+	}else if this.Dbtype=="mysql"{
+		return this.getMysqlType(v)
+	}
+	return ""
 }
 
 func (this *Dialect) getSqliteType(i interface{}) string{
-     switch i.(type){
-        case int:
-            return "integer"
-        case float64:
-            return "float"
-        case string:
-            return "text"
-        case bool:
-            return "integer"
-        case []byte:
-            return "blob"
-        // case time.Time:
-            // return "datetime"
-        default:
-            return "text"
-    }
+	switch i.(type){
+	case int:
+		return "integer"
+	case float64:
+		return "float"
+	case string:
+		return "text"
+	case bool:
+		return "integer"
+	case []byte:
+		return "blob"
+	// case time.Time:
+	// return "datetime"
+	default:
+		return "text"
+	}
 }
 func (this *Dialect) getMysqlType(i interface{}) string{
-     switch i.(type){
-        case int:
-            return "INT(11)"
-        case float64:
-            return "DOUBLE"
-        case string:
-            return "VARCHAR(255)"
-        case bool:
-            return "TINYINT"
-        case []byte:
-            return "BLOB"
-        // case time.Time:
-            // return "datetime"
-        default:
-            return "text"
-    }
+	switch i.(type){
+	case int:
+		return "INT(11)"
+	case float64:
+		return "DOUBLE"
+	case string:
+		return "VARCHAR(255)"
+	case bool:
+		return "TINYINT"
+	case []byte:
+		return "BLOB"
+	// case time.Time:
+	// return "datetime"
+	default:
+		return "text"
+	}
 }
-
-
